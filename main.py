9697195f2fe29cf3fbf3210c98d62dcdfa6673f6
antoninakus <antonina.kus123@gmail.com>
2025-03-11 19:55:05 +0100

    Fix sending ping so it ignores missed packages and only reports actual issues

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import ping3
 import time
@@ -6,6 +7,7 @@ import requests
 
 def ping_ip(url, args):
     print(url, args)
+    timeout = os.getenv("TIMEOUT", 15)
     last_res = {}
     res = {}
 
@@ -13,19 +15,27 @@ def ping_ip(url, args):
         last_res[arg] = (ping3.ping(arg))
 
     while True:
-        time.sleep(10)
+        time.sleep(int(timeout))
         for arg in args:
             res[arg] = ping3.ping(arg)
 
             if (not last_res[arg]) != (not res[arg]):
-                data = {"content": ""}
+                data = {"content": None}
 
                 if not res[arg]:
-                    data["content"] = f"{arg} is down!"
+                    is_down = True
+                    for i in range(5):
+                        time.sleep(int(timeout))
+                        res[arg] = ping3.ping(arg)
+                        if res[arg]:
+                            is_down = False
+                    if is_down:
+                        data["content"] = f"{arg} is down!"
                 else:
                     data["content"] = f"{arg} is back up!"
 
-                requests.post(url, json=data)
+                if data["content"]:
+                    requests.post(url, json=data)
 
             last_res[arg] = res[arg]
 
@@ -35,4 +45,5 @@ if __name__ == "__main__":
         print("Usage: python3 main.py <webhook_url> <ip1> <ip2> ...")
         sys.exit(1)
     else:
+
         ping_ip(sys.argv[1], sys.argv[2:])
